5cc5860e4190b55c17f9637fb04a02d791a3f19b
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-06 20:09:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function change() {
+	let paragraph = document.getElementById("status")
+	let h1 = document.createElement("h1")
+	h1.innerHTML = paragraph.innerHTML
+	paragraph.replaceWith(h1)
+}
