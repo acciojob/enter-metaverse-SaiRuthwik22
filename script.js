d22952d871f8f91dbfbcad25c84fefe29e2b407f
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-06 20:13:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 function change() {
 	let paragraph = document.getElementById("status")
 	let h1 = document.createElement("h1")
-	h1.innerHTML = paragraph.innerHTML
+	h1.innerHTML = "Entered Metaverse"
 	paragraph.replaceWith(h1)
 }
