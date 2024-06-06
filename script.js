7393784004b91eb60969fcc32bc183109eedd611
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-06 20:16:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ function change() {
 	let paragraph = document.getElementById("status")
 	let h1 = document.createElement("h1")
 	h1.innerHTML = "Entered Metaverse"
+	h1.id = "status"
 	paragraph.replaceWith(h1)
 }
